152dcf8da8842d0867e87334d8b53db0f6fc76f7
Sebastian Alfieri <sebasalfieri@gmail.com>
2023-08-08 16:52:30 -0300

    a

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+import { ResultContainer } from '@/containers';
+
+export default function Home() {
+  return (
+    <main>
+      <ResultContainer />
+    </main>
+  );
+}
