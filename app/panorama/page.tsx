bedf1d4a3430423bfe70785a51bc05e6714b504e
Sebastian Alfieri <sebasalfieri@gmail.com>
2025-09-23 13:28:15 -0300

    wip

--- app/panorama/page.tsx
+++ app/panorama/page.tsx
@@ -9,13 +9,28 @@ export default function PanoramaPage() {
       id: 'scene-a',
       image: '/images/panorama0.jpg',
       initialView: { yaw: 0, pitch: 0, fov: 75 },
-      hotspots: [{ id: 'to-b', yaw: 45, pitch: 0, label: 'Ir a B', target: 1 }]
+      hotspots: [
+        { id: 'to-b', yaw: 45, pitch: 0, label: 'Ir a B', target: 1 },
+        { id: 'to-b', yaw: 205, pitch: 0, label: 'Ir a B', target: 2 }
+      ]
     },
     {
       id: 'scene-b',
       image: '/images/panorama1.jpg',
       initialView: { yaw: -30, pitch: 0, fov: 75 },
-      hotspots: [{ id: 'to-a', yaw: -135, pitch: 0, label: 'Volver a A', target: 0 }]
+      hotspots: [
+        { id: 'to-a', yaw: -135, pitch: 0, label: 'Volver a A', target: 0 },
+        { id: 'to-a', yaw: 0, pitch: 0, label: 'Volver a A', target: 2 }
+      ]
+    },
+    {
+      id: 'scene-c',
+      image: '/images/panorama2.jpg',
+      initialView: { yaw: -30, pitch: 0, fov: 75 },
+      hotspots: [
+        { id: 'to-a', yaw: -135, pitch: 0, label: 'Volver a A', target: 0 },
+        { id: 'to-a', yaw: 135, pitch: 0, label: 'Volver a A', target: 1 }
+      ]
     }
   ];
   return (
