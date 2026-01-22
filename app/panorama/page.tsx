2c6b6f6cc9ca29061767b1c27a86adcac5df9c18
Sebastian Alfieri <sebasalfieri@gmail.com>
2026-01-22 15:35:28 -0300

    yaw

--- app/panorama/page.tsx
+++ app/panorama/page.tsx
@@ -10,7 +10,7 @@ export default function PanoramaPage() {
     {
       id: '1',
       image: '/images/dji_export_20260121_152950_1769030990346_sphere_screenshot.jpg',
-      initialView: { yaw: 180, pitch: 0, fov: 75 },
+      initialView: { yaw: 0, pitch: 0, fov: 75 },
       hotspots: [
         // { id: 'to-b', yaw: 45, pitch: 0, label: 'Ir a B', target: 1 },
         { id: 'to-b', yaw: 205, pitch: 0, label: 'Ir a B', target: 1 }
@@ -19,7 +19,7 @@ export default function PanoramaPage() {
     {
       id: '2',
       image: '/images/dji_fly_20260121_150604_54_1769030367509_pano_optimized.jpg',
-      initialView: { yaw: -30, pitch: 0, fov: 75 },
+      initialView: { yaw: 0, pitch: 0, fov: 75 },
       hotspots: [
         { id: 'to-a', yaw: 0, pitch: 50, label: 'Volver a A', target: 0 },
         { id: 'to-a', yaw: 0, pitch: 0, label: 'Volver a A', target: 2 }
@@ -28,7 +28,7 @@ export default function PanoramaPage() {
     {
       id: '3',
       image: '/images/dji_fly_20260121_150718_55_1769030313699_pano_optimized.jpg',
-      initialView: { yaw: -30, pitch: 0, fov: 75 },
+      initialView: { yaw: 0, pitch: 0, fov: 75 },
       hotspots: [
         { id: 'to-a', yaw: -135, pitch: 0, label: 'Volver a A', target: 1 },
         { id: 'to-a', yaw: 135, pitch: 0, label: 'Volver a A', target: 3 }
@@ -37,7 +37,7 @@ export default function PanoramaPage() {
     {
       id: '4',
       image: '/images/dji_fly_20260121_150838_56_1769030691914_pano_optimized.jpg',
-      initialView: { yaw: -30, pitch: 0, fov: 75 },
+      initialView: { yaw: 0, pitch: 0, fov: 75 },
       hotspots: [
         { id: 'to-a', yaw: -135, pitch: 0, label: 'Volver a A', target: 2 },
         { id: 'to-a', yaw: 135, pitch: 0, label: 'Volver a A', target: 4 }
@@ -46,7 +46,7 @@ export default function PanoramaPage() {
     {
       id: '5',
       image: '/images/dji_fly_20260121_150942_57_1769031104219_pano_optimized.jpg',
-      initialView: { yaw: -30, pitch: 0, fov: 75 },
+      initialView: { yaw: 0, pitch: 0, fov: 75 },
       hotspots: [
         { id: 'to-a', yaw: -135, pitch: 0, label: 'Volver a A', target: 3 },
         { id: 'to-a', yaw: 135, pitch: 0, label: 'Volver a A', target: 5 }
